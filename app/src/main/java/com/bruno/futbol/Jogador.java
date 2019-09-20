b31aa8a61d2c56f62b992328884c33d5be5fad8d
Bruno Alves <brunovicentealves.ti@gmail.com>
2019-09-20 09:59:26 -0300

    criado o classe Jogador

---
+++ app/src/main/java/com/bruno/futbol/Jogador.java
@@ -0,0 +1,43 @@
+package com.bruno.futbol;
+
+public class Jogador {
+
+    private int idjogador;
+    private String nomejogador;
+    private String posicao;
+    private int  idtime;
+
+    public int getIdjogador() {
+        return idjogador;
+    }
+
+    public void setIdjogador(int idjogador) {
+        this.idjogador = idjogador;
+    }
+
+    public String getNomejogador() {
+        return nomejogador;
+    }
+
+    public void setNomejogador(String nomejogador) {
+        this.nomejogador = nomejogador;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public int getIdtime() {
+        return idtime;
+    }
+
+    public void setIdtime(int idtime) {
+        this.idtime = idtime;
+    }
+
+
+}
