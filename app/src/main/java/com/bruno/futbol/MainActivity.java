ec8607c7eec28e3e2c8b152f564ebd6c77ee5bb7
Bruno Alves <brunovicentealves.ti@gmail.com>
2019-09-23 15:36:39 -0300

    foi feito modificações no formulario e realizado metodo para  ir para tela de cadastro

--- app/src/main/java/com/bruno/futbol/MainActivity.java
+++ app/src/main/java/com/bruno/futbol/MainActivity.java
@@ -1,9 +1,9 @@
 package com.bruno.futbol;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -25,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, Formulario_TimeActivity.class);
+                startActivity( intent );
             }
         });
     }
@@ -51,5 +51,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+
     }
 }
