23322d5c2f5b82b925567e776c1995ea75171955
Bruno Alves <brunovicentealves.ti@gmail.com>
2019-09-23 15:31:35 -0300

    criado formulario de cadastro do time

---
+++ app/src/main/java/com/bruno/futbol/Formulario_TimeActivity.java
@@ -0,0 +1,14 @@
+package com.bruno.futbol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formulario_TimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario__time);
+    }
+}
